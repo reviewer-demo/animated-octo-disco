30e004074929ffc65c40363845dd181b75a5e205
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2026-04-13 20:40:59 +0200

    Revert implementation

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,4 @@ struct Arguments {
     b: i32,
 }
 
-struct Adder;
-
-impl Adder {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn add(&self, a: i32, b: i32) -> i32 {
-        a + b + a
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::Adder;
-
-    #[test]
-    fn test_add_two_numbers() {
-        let adder = Adder::new();
-
-        let res = adder.add(3, 4);
-        assert_eq!(7, res);
-    }
-}
-
-fn main() {
-    let args = Arguments::parse();
-    let adder = Adder::new();
-    let res = adder.add(args.a, args.b);
-    println!("{} + {} = {res}", args.a, args.b);
-}
+fn main() {}
